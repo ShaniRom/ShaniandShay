033d6ebf07e33ef228ee1796a7eb1c302c9e27f8
Shani <shanaynayrom@gmail.com>
2022-12-06 20:04:54 +0200

    added folders and relearning redux

---
+++ client/my-app/src/App2.tsx
@@ -0,0 +1,18 @@
+ import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+// import './App.css';
+
+// import PracticeComponent from './practiceComponent';
+
+// function App() {
+//   return (
+//     <div className="App">
+     
+//       <PracticeComponent/>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
