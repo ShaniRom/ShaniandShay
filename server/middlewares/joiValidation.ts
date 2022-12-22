59d65ddfb016da23e5db5aafc36ed617f2251717
Shani <shanaynayrom@gmail.com>
2022-12-22 14:54:34 +0200

    joi

--- server/middlewares/joiValidation.ts
+++ server/middlewares/joiValidation.ts
@@ -51,7 +51,7 @@
 module.exports = (validator) => {
   return (req, res, next) => {
     const { error } = validator(req.body)
-    console.log('error: ', error)
+    console.error('error: ', error)
     if (error) {
       return res.status(400).send(error.details[0].message)
     }
