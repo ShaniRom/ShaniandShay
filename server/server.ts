955a18154bb6b6aa18602cd3719168cbd165e08c
Shani <shanaynayrom@gmail.com>
2022-12-22 12:21:52 +0200

    fixing server client connection

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 const app = express();
-const port = process.env.PORT || 4001;
+const port: number =  4001;
 
 app.use(express.static('public/build'));
 app.use(express.json());
 
-//MONGODB_URI="mongodb+srv://ShaniRom:CakFWbktCxPEqGS3@cluster0.vh1hg.mongodb.net/airbnb-clone?retryWrites=true&w=majority"
+
 
 mongoose
   .connect(
